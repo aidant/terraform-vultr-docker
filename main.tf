748a6a49b094e44159a07da0cb82363909e81a10
aidant <15520814+aidant@users.noreply.github.com>
2022-08-17 22:05:43 +1000

    Set y & q flags on apt-get

--- main.tf
+++ main.tf
@@ -44,16 +44,16 @@ resource "vultr_instance" "server" {
       "set -euo pipefail",
       "export DEBIAN_FRONTEND=noninteractive",
 
-      "apt-get update",
-      "apt-get upgrade",
+      "apt-get -yq update",
+      "apt-get -yq upgrade",
 
       # Install Docker
-      "apt-get install ca-certificates curl gnupg lsb-release",
+      "apt-get -yq install ca-certificates curl gnupg lsb-release",
       "mkdir -p /etc/apt/keyrings",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
       "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-      "apt-get update",
-      "apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin",
+      "apt-get -yq update",
+      "apt-get -yq install docker-ce docker-ce-cli containerd.io docker-compose-plugin",
 
       # Create User
       "adduser --disabled-password --gecos \"\" ${var.username}",
